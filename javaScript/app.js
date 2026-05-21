ac1aae29a655bae536da58b0acac03d722a5a0ea
Jhonayker777 <jhon117.hello@gmail.com>
2026-05-20 19:14:55 -0500

    Lectura de inputs

--- javaScript/app.js
+++ javaScript/app.js
@@ -1,2 +1,12 @@
+
+"use strict";
+
 const nombreRuta = document.getElementById("rutaName")
-const nombreRuta = document.getElementById("rutaName")
+const nombreConductor = document.getElementById("driveName")
+const horaSalida = document.getElementById("departureTime")
+const nombreEstudiante = document.getElementById("studentName")
+const selectorRuta = document.getElementById("routeSelect")
+const infoClima = document.getElementById("weatherWindget")
+const ContenedorRutas = document.getElementById("routersContainer")
+const btnNuevo = document.getElementById("btnNuevo")
+
